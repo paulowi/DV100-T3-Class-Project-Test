eaf53840dc4dda0799f5401f027a292ce89f36b6
paulowi <81673350+paulowi@users.noreply.github.com>
2023-07-14 16:06:48 +0200

    Week 2 Class Exercise

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// When the document loads
+$(document).ready(function(){
+
+    console.log("Hello");
+
+    // When the document loads, animate the hero image upwards
+    $("#hero-image").animate({top: '-=100px'});
+  }); 
