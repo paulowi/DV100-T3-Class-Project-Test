d40c0f6ed00f862465fb6c37004ea43114f9c957
paulowi <81673350+paulowi@users.noreply.github.com>
2023-07-20 19:08:11 +0200

    Add week 3 content

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,28 @@ $(document).ready(function(){
 
     console.log("Hello");
 
+    // -----------------------------------------
+    // Home Page
+
     // When the document loads, animate the hero image upwards
-    $("#hero-image").animate({top: '-=100px'});
+    $(".hero-image").animate({top: '-=100px'});
+
+    // -----------------------------------------
+    // Browse Page
+
+    // Hide all description text from the plant cards
+    $("#descriptionText").hide();
+
   }); 
+
+  // When the card is clicked
+  $(".card").click(function(){
+
+    // Toggle the price & description text
+    $("#priceText").toggle();
+    $("#descriptionText").toggle();
+
+    // Resize the image to fit the additional content
+    $(".card-img-top").toggleClass("small");
+
+  });
